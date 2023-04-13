5988bc0da002235962e0fac5aaf1bdc4ed6dddaa
D00MTrooper <122827195+D00MTrooper@users.noreply.github.com>
2023-04-13 21:05:30 +0200

    updated version
    nearly working decimal to binary

--- IPsubnet.cpp
+++ IPsubnet.cpp
@@ -1,27 +1,71 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-    int liczba, bit, i;
-    int binary[32]; // maksymalna liczba bitÛw potrzebna do zapisu liczby ca≥kowitej
-    
-    cout << "Podaj liczbÍ w systemie dziesiÍtnym: ";
-    cin >> liczba;
+typedef struct ip {
+    int octet_decimal;
+    string octet_binary;
+} ip;
 
-    // zamiana na postaÊ binarnπ
-    for (i = 0; liczba > 0; i++) {
-        bit = liczba % 2;
-        binary[i] = bit;
-        liczba /= 2;
+ip orginal[4];
+string binary;
+bool flaga1;
+
+void ToBinary(int decimal)
+{
+    int bit, i, j, amount, to_add;
+    int temp_binary[8]; // maksymalna liczba bit√≥w potrzebna do zapisu liczby ca≈Çkowitej
+
+    if ((decimal != 0) && (decimal < 255)) {
+        // zamiana na postaƒá binarnƒÖ
+        for (i = 0; decimal > 0; i++) {
+            bit = decimal % 2;
+            temp_binary[i] = bit;
+            decimal /= 2;
+        }
+
+        // konwersja na string
+        binary = "";
+        for (j = i - 1; j >= 0; j--) {
+            binary += to_string(temp_binary[j]);
+        }
+
+        amount = binary.length();
+
+        if (amount < 8) {
+            to_add = 8 - amount;
+            string supplement(to_add, '0');
+            binary = supplement + binary;
+        }
+    } else if (decimal == 0) {
+        binary = "00000000";
+    }else if (decimal > 255) {
+        flaga1 = true;
+    }
+}
+
+int main()
+{
+    int liczba, i;
+
+    for (i = 0; i <= 3; i++) {
+        cout << "\nPodaj " << i + 1 << " oktet: ";
+        cin >> orginal[i].octet_decimal;
+
+        if (flaga1 == true) {
+            cout << "\n za duzo";
+            i--;
+        } else {
+            ToBinary(orginal[i].octet_decimal);
+            cout << "\nOktet " << i + 1 << ": " << binary;
+            orginal[i].octet_binary = binary;
+            flaga1 = false;
+        }
+
     }
 
-    // wyúwietlenie wyniku
-    cout << "Liczba w systemie binarnym: ";
-    for (i--; i >= 0; i--) {
-        cout << binary[i];
-    }
-    cout << endl;
+    cout << "\nAdres IP w systemie dziesiƒôtnym: " << orginal[0].octet_decimal << "." << orginal[1].octet_decimal << "." << orginal[2].octet_decimal << "." << orginal[3].octet_decimal;
+    cout << "\nAdres IP w systemie binarnym: " << orginal[0].octet_binary << "." << orginal[1].octet_binary << "." << orginal[2].octet_binary << "." << orginal[3].octet_binary;
 
     return 0;
 }
-
